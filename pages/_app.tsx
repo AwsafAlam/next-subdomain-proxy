bffadf051e85266c9ed227f39641c1b98555c74c
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2023-01-07 19:59:25 +0600

    basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,16 +7,13 @@ import '../styles/globals.css';
 import 'assets/template/App.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
-import PageLoader from 'components/PageLoader';
-import Script from 'next/script';
-import config from 'utils/config';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const handleStart = (url: string) => {
+    const handleStart = () => {
       NProgress.start();
       setLoading(true);
     };
@@ -35,39 +32,18 @@ function MyApp({ Component, pageProps }: AppProps) {
       router.events.off('routeChangeError', handleStop);
     };
   }, [router]);
-  const { city, state, living_type, slug } = router.query;
-  return <>{loading ? <PageLoader /> : (<>
-    {config.DEPLOYMENT === 'prod' ?
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PLXWRW3');
-      `}
-      </Script> : null
-    }
 
-    {config.DEPLOYMENT === 'prod' ?
-      <>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-156857230-1" />
-        <Script id="gtag">
-          {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)};
-        gtag('js', new Date());
-        ${slug ? `gtag('set', {'content_group3': 'Property page'});` : city ? `gtag('set', {'content_group1': 'City Page'});` : state ? `gtag('set', {'content_group2': 'State Page'});` : living_type ? `gtag('set', {'content_group4': 'Pillar Page'});` : ''}  
-
-        gtag('config', 'UA-156857230-1');
-        `}
-
-        </Script>
-      </>
-      : null}
-
-
-    <Component {...pageProps} />
-  </>)}</>;
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <Component {...pageProps} />
+        </>
+      )}
+    </>
+  );
 }
 
 export default MyApp;
